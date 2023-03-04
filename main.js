e0ea6cd5eab3b0bbc61b1a89efac38329acd9d1b
Tivotal <121335284+Tivotal@users.noreply.github.com>
2023-03-04 14:34:18 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 /* Created by Tivotal */
+
+const hamburger_menu = document.querySelector(".hamburger-menu");
+const container = document.querySelector(".container");
+
+hamburger_menu.addEventListener("click", () => {
+  container.classList.toggle("active");
+});
